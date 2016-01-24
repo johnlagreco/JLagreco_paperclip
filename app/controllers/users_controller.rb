2a43ed81aaa64dfd5b4ceac4c93afa9be77e4114
John LaGreco <johnlagreco@gmail.com>
2016-01-24 01:19:09 -0500

    Added erb files & modified rb files.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
   def index
+  	@user = User.all
   end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+  	@user = User.find(params[:id])
+    @user.destroy
+      redirect_to users_path
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :avatar)
+    end
 end
